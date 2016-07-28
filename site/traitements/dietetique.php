fadf123e94f8e7238e49e44233caae887667f5d1
Nakawashi <l.genevey@gmail.com>
2016-07-28 10:51:30 +0200

    ajout du dossier /site

---
+++ site/traitements/dietetique.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+
+  <title>Traitements - Moxibustion</title>
+
+  <meta name="description" content="">
+  <meta name="author" content="lucie genevey">
+
+    <!-- Bootstrap Core CSS -->
+    <link rel="stylesheet" href="../css/app.css">
+    
+    <!-- Bootstrap Flat Ui -->
+    <link href="dist/css/flat-ui.min.css" rel="stylesheet">
+    
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="../css/general.css">
+    <link rel="stylesheet" href="../css/moxibustion.css">
+	
+
+    <!--[if lt IE 9]>
+    <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+    
+</head>
+<body>
+
+    <header class="header">
+        <div class="container">
+        
+            <div class="row">
+                <div class="col-sm-4 left-picture header-picture">
+                    <img alt="symbole chinois" src="../images/img-header/img-gauche.png">
+                </div>
+
+                <div class="col-sm-4 div-header-title">
+                    <h1 class="header-title1">David Riemarzik</h1>
+                    <hr class="hr-header-subtitle">
+                    <h2 class="header-title2">Acupuncture</h2>
+                </div>
+
+                <div class="col-sm-4 right-picture header-picture">
+                    <img alt="symbole chinois" src="../images/img-header/img-droite.png">
+                </div>
+            </div>
+        
+            <div class="row">
+                <div class="col-sm-12 navigation">
+                    <nav class="menu">
+                        <ul class="menu-list">
+                            <li class="menu-inline"><a class="btn active" href="../index.php">Accueil</a></li>
+                            <li class="menu-inline dropdown"><a class="dropdown-toggle btn" data-toggle="dropdown">Traitements <span class="caret"></span></a>
+                                <ul class="dropdown-menu">
+                                    <li><a href="acupuncture.php">Acupuncture</a></li>
+                                    <li><a href="moxibustion.php">Moxibustion</a></li>
+                                    <li><a href="tuina.php">Tui Na (massages chinois)</a></li>
+                                    <li><a href="ventouses.php">Ventouses</a></li>
+                                    <li><a href="phytotherapie.php">Phytothérapie</a></li>
+                                    <li><a href="dietetique.php">Conseil Diététique</a></li>
+                                </ul>
+                            </li>
+                            <li class="menu-inline"><a class="btn" href="../praticien.php">Praticien</a></li>
+                            <li class="menu-inline"><a class="btn" href="../maladies-traitees.php">Maladies Traitées</a></li>
+                            <li class="menu-inline"><a class="btn" href="../tarifs-et-remboursement.php">Tarifs et Remboursements</a></li>
+                            <li class="menu-inline"><a class="btn" href="../contact.php">Contact</a></li>
+                            <li class="menu-inline"><a class="btn" href="../liens.php">Liens</a></li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+            
+        </div>
+    </header>
+    
+    <div class="container">
+        <div class="row">
+            
+            <div class="col-md-12">
+                <h2>Conseils diététiques</h2>
+                
+                <p>Thérapie par l’alimentation du point de vue de la médecine traditionnelle chinoise (MTC) 
+                <br>
+                <br>
+                Des propositions d’alimentation individuelles sont établies sur la base d’un diagnostic du pouls et de la langue ainsi que d’un interrogatoire intensif du patient. Les caractéristiques énergétiques des aliments et leurs effets sur l’organisme sont utilisés pour supprimer les déséquilibres dans le flux énergétique (Qi). 
+</p>
+                
+            </div>
+            
+            <div class="col-md-12 img-moxibustion-position">
+                <img src="../images/img-traitements/img-dietetique.jpg">
+            </div>
+            
+        </div>
+    </div>
+    
+<?php include_once('../include/footer.inc.html') ?>
---
+++ site/traitements/tuina.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+
+  <title>Traitements - Tui Na</title>
+
+  <meta name="description" content="">
+  <meta name="author" content="lucie genevey">
+
+    <!-- Bootstrap Core CSS -->
+    <link rel="stylesheet" href="../css/app.css">
+    
+    <!-- Bootstrap Flat Ui -->
+    <link href="dist/css/flat-ui.min.css" rel="stylesheet">
+    
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="../css/general.css">
+    <link rel="stylesheet" href="../css/moxibustion.css">
+	
+
+    <!--[if lt IE 9]>
+    <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+    
+</head>
+<body>
+
+    <header class="header">
+        <div class="container">
+        
+            <div class="row">
+                <div class="col-sm-4 left-picture header-picture">
+                    <img alt="symbole chinois" src="../images/img-header/img-gauche.png">
+                </div>
+
+                <div class="col-sm-4 div-header-title">
+                    <h1 class="header-title1">David Riemarzik</h1>
+                    <hr class="hr-header-subtitle">
+                    <h2 class="header-title2">Acupuncture</h2>
+                </div>
+
+                <div class="col-sm-4 right-picture header-picture">
+                    <img alt="symbole chinois" src="../images/img-header/img-droite.png">
+                </div>
+            </div>
+        
+            <div class="row">
+                <div class="col-sm-12 navigation">
+                    <nav class="menu">
+                        <ul class="menu-list">
+                            <li class="menu-inline"><a class="btn active" href="../index.php">Accueil</a></li>
+                            <li class="menu-inline dropdown"><a class="dropdown-toggle btn" data-toggle="dropdown">Traitements <span class="caret"></span></a>
+                                <ul class="dropdown-menu">
+                                    <li><a href="acupuncture.php">Acupuncture</a></li>
+                                    <li><a href="moxibustion.php">Moxibustion</a></li>
+                                    <li><a href="tuina.php">Tui Na (massages chinois)</a></li>
+                                    <li><a href="ventouses.php">Ventouses</a></li>
+                                    <li><a href="phytotherapie.php">Phytothérapie</a></li>
+                                    <li><a href="dietetique.php">Conseil Diététique</a></li>
+                                </ul>
+                            </li>
+                            <li class="menu-inline"><a class="btn" href="../praticien.php">Praticien</a></li>
+                            <li class="menu-inline"><a class="btn" href="../maladies-traitees.php">Maladies Traitées</a></li>
+                            <li class="menu-inline"><a class="btn" href="../tarifs-et-remboursement.php">Tarifs et Remboursements</a></li>
+                            <li class="menu-inline"><a class="btn" href="../contact.php">Contact</a></li>
+                            <li class="menu-inline"><a class="btn" href="../liens.php">Liens</a></li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+            
+        </div>
+    </header>
+    
+    <div class="container">
+        <div class="row">
+            
+            <div class="col-md-12">
+                <h2>TUI NA (massages chinois)</h2>
+                
+                <p>Le massage Tuina fait partie des formes de traitement les plus anciennes de la médecine traditionnelle chinoise. Avec les mains, les doigts, les coudes ou les pieds on exerce une pression et un frottement sur les points douloureux du corps. En outre, par les méridiens (voies de transmission de l’énergie) il est possible d’influencer positivement les organes internes.
+                <br>
+                <br>
+                Le massage Tuina stimule circulation sanguine, mobilise les articulations raidies et réchauffe certaines parties du corps, ce qui influence aussi les organes internes.</p>
+                
+            </div>
+            
+            <div class="col-md-12 img-moxibustion-position">
+                <img src="../images/img-traitements/img-tuina.jpg">
+            </div>
+            
+        </div>
+    </div>
+    
+<?php include_once('../include/footer.inc.html') ?>
